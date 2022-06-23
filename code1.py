f5c3df0f63429ed5cb1b022fffdbd665ba956efe
Alekya <alekya.arra@somaiya.edu>
2022-06-23 08:29:10 +0530

    taking inputs

--- code1.py
+++ code1.py
@@ -1,2 +1,21 @@
-print("hello")
-print("a+b")
+from tkinter import *
+
+root = Tk()
+root.title("diet")
+
+l1 = Label(text='enter age')
+l2 = Label(text='enter weight')
+l3 = Label(text='enter colorie intake per day')
+
+l1.grid(row=0,column=0, sticky='w')
+l2.grid(row=1,column=0, sticky='w')
+l3.grid(row=2,column=0, sticky='w')
+
+age=Entry().grid(row=0,column=1)
+weight=Entry().grid(row=1,column=1)
+Calories=Entry().grid(row=2,column=1)
+root.mainloop()
+
+
+
+
