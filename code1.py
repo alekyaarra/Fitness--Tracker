c63afcd238c7ebc7395d82f4b87b57d1cafcbc2d
Alekya <alekya.arra@somaiya.edu>
2022-06-28 12:26:54 +0530

    edited code1

--- code1.py
+++ code1.py
@@ -5,7 +5,7 @@ root.title("diet")
 
 l1 = Label(text='enter age')
 l2 = Label(text='enter weight')
-l3 = Label(text='enter colorie intake per day')
+l3 = Label(text='enter colorie intake pe day')
 
 l1.grid(row=0,column=0, sticky='w')
 l2.grid(row=1,column=0, sticky='w')
@@ -14,9 +14,9 @@ l3.grid(row=2,column=0, sticky='w')
 age=Entry().grid(row=0,column=1)
 weight=Entry().grid(row=1,column=1)
 Calories=Entry().grid(row=2,column=1)
-root.mainloop()
 
-print(age.get())
+
+root.mainloop()
 
 
 
