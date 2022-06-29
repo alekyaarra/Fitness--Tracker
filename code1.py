31c613c9cddcf0dda3d09e0b187765028ec54256
Alekya <alekya.arra@somaiya.edu>
2022-06-29 07:43:44 +0530

    edited the perivious code as it wasn't working properly

--- code1.py
+++ code1.py
@@ -3,24 +3,23 @@ from tkinter import ttk
 
 def weight_loss():
     my_tree=ttk.Treeview(root)
-    my_tree['columns']=('food_pre_breakfast','food_breakfast','food_pre_lunch','food_lunch','food_post_lunch','food_snacks', 'food_dinner')
+    my_tree['columns']=('food_pre_breakfast','food_breakfast','food_pre_lunch','food_lunch','food_snacks', 'food_dinner')
 
     my_tree.column("#0", width=0,  stretch=NO)
-    my_tree.column("food_pre_breakfast",anchor=CENTER, width=80)
-    my_tree.column("food_breakfast",anchor=CENTER, width=80)
-    my_tree.column("food_pre_lunch",anchor=CENTER, width=80)
-    my_tree.column("food_lunch",anchor=CENTER, width=80)
-    my_tree.column("food_snacks",anchor=CENTER, width=80)
-    my_tree.column("food_dinner",anchor=CENTER, width=80)
-    print("a")
-    my_tree.column("#0", text='', anchor=CENTER)
-    my_tree.column("food_pre_breakfast",text="Pre Breakfast", anchor=CENTER, width=80)
-    my_tree.column("food_breakfast",text='Breakfast', anchor=CENTER, width=80)
-    my_tree.column("food_pre_lunch",text='Pre Lunch', anchor=CENTER, width=80)
-    my_tree.column("food_lunch",text='Lunch', anchor=CENTER, width=80)
-    my_tree.column("food_snacks",text='Snacks', anchor=CENTER, width=80)
-    my_tree.column("food_dinner",text='Dinner', anchor=CENTER, width=80)
-
+    my_tree.column("food_pre_breakfast",anchor=CENTER, width=150)
+    my_tree.column("food_breakfast",anchor=CENTER, width=150)
+    my_tree.column("food_pre_lunch",anchor=CENTER, width=150)
+    my_tree.column("food_lunch",anchor=CENTER, width=150)
+    my_tree.column("food_snacks",anchor=CENTER, width=150)
+    my_tree.column("food_dinner",anchor=CENTER, width=300)
+    
+    my_tree.heading("#0", text='', anchor=CENTER)
+    my_tree.heading("food_pre_breakfast",text="Pre Breakfast", anchor=CENTER)
+    my_tree.heading("food_breakfast",text='Breakfast', anchor=CENTER)
+    my_tree.heading("food_pre_lunch",text='Pre Lunch', anchor=CENTER)
+    my_tree.heading("food_lunch",text='Lunch', anchor=CENTER)
+    my_tree.heading("food_snacks",text='Snacks', anchor=CENTER)
+    my_tree.heading("food_dinner",text='Dinner', anchor=CENTER)
 
     food=[
         ['Cucumber Detox Water','Oats Porridge in Skimmed Milk','Skimmed Milk Panner','Dal,Gajar Matar Sabzi,Roti','Cut Fruits, Buttermilk','Dal,Lauki Sabzi,Roti'],
@@ -41,7 +40,7 @@ def weight_loss():
 
 
 root = Tk()
-root.geometry('600x600')
+root.state("zoomed")
 root.title('diet plan')
 
 #my_tree=ttk.Treeview(root)
